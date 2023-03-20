c6b0899cf3f4ced53a24c145b728a0da32403889
Peilun Huang <phuang61@my.bcit.ca>
2023-03-20 14:36:45 -0700

    default arg

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,18 @@
 
 // ___________________________________
 
-let greet = (user) => {
-    console.log(`Hello ${user}`);
-}
-greet('L')
+// let greet = (user) => {
+//     console.log(`Hello ${user}`);
+// }
+// greet('L')
+
+// ____________________________
+// function greet(user, time = 'day'){
+//     console.log(`Good ${time} ${user}`);
+// }
+
+// greet("Pei", "night")
+
+// ____________________________
+
+
